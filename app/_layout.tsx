d5d8e19df42d61d9fdea44b74bba7ac95768c6a5
Olivier <chob.olivier@gmail.com>
2026-07-31 16:05:14 +0200

    Remplacement clé API RevenueCat test par clé de production

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   useEffect(() => {
-    Purchases.configure({ apiKey: 'test_IJQthZrPVwoYxXWfnptSItXrslu' });
+    Purchases.configure({ apiKey: 'goog_EmzMeCeEJtxQxrpAhUXLLdRGQWM' });
   }, []);
 
   return (
